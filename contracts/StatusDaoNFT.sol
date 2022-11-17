df6deebf75a0d68a149c0f425b1af1b616808eeb
[nobonobo1256] <[noboru1256@gmail.com]>
2022-11-17 12:42:07 +0900

    更新

--- contracts/StatusDaoNFT.sol
+++ contracts/StatusDaoNFT.sol
@@ -1,7 +1,32 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract StatusDaoNFT {
+contract StatusDaoNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
+    constructor() ERC721("StatusDaoNFT", "SDN"){}
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+     ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId) ;
+    }
     
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
 }
